8139817628b1c17ba782fedcbcafb1279fe13b49
Rasha Hantash <rasha.hantash01@gmail.com>
2024-10-15 22:04:05 -0400

    light / darkmode

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from "next/image";
+import ThemeSwitcher from "../component/ThemeSwitcher";
+
+export default function Home() {
+
+  return (
+    <div>
+      <ThemeSwitcher />     
+    </div>
+  );
+}
---
+++ frontend/component/ThemeSwitcher.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { Sun, Moon } from 'lucide-react';
+
+const ThemeSwitcher = () => {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const toggleTheme = () => {
+    if (currentTheme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
+  return (
+    <div className="transition-colors duration-700">
+      <button
+        onClick={toggleTheme}
+        className="p-2 rounded-full transition-colors duration-700"
+        aria-label="Toggle theme"
+      >
+        {currentTheme === 'dark' ? (
+          <Sun className="w-6 h-6 text-white" />
+        ) : (
+          <Moon className="w-6 h-6 text-gray-700" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
