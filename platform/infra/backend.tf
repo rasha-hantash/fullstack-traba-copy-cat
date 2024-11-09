565c25dab8c710fbb232a3fa6f681a1b89084234
Rasha Hantash <rasha.hantash01@gmail.com>
2024-11-09 14:31:21 -0500

    move terraform-state to s3

---
+++ platform/infra/backend.tf
@@ -0,0 +1,10 @@
+# backend.tf
+terraform {
+  backend "s3" {
+    bucket         = "traba-terraform-states"  
+    key            = "fullstack-infra/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-locks" 
+  }
+}
